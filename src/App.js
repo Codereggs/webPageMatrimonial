d1c01042ef809105fdc98fb46ce8249a148eb76d
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-10-09 10:38:23 -0300

    feat temporizador

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Fecha from "./Components/Fecha";
+import Inicio from "./Components/Inicio";
+import RedesS from "./Components/RedesS";
+import Regalos from "./Components/Regalos";
+import Reserva from "./Components/Reserva";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Inicio />
+      <Fecha />
+      <RedesS />
+      <Regalos />
+      <Reserva />
+    </>
+  );
 }
 
 export default App;
--- src/Components/Inicio.js
+++ src/Components/Inicio.js
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import { temporizador } from "./Temporizador";
+
 const Inicio = () => {
-  return <div>Inicio</div>;
+  useEffect(() => {
+    temporizador(2021, 12, 11, 0, 0, 0);
+  }, []);
+
+  return (
+    <div>
+      Inicio
+      <p className="count"></p>
+    </div>
+  );
 };
 
 export default Inicio;
