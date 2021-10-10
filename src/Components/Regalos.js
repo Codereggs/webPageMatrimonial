059002de2b1796d486ecd51958dd264043260651
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-10-10 09:10:06 -0300

    feat regalos

--- src/Components/Regalos.js
+++ src/Components/Regalos.js
@@ -1,5 +1,17 @@
+import { Typography } from "@mui/material";
+import { GrGift } from "react-icons/gr";
+import "./Regalos.css";
+
 const Regalos = () => {
-  return <div>Regalos</div>;
+  return (
+    <section className="regalos">
+      <GrGift className="giftIcon" />
+      <Typography variant="h2">
+        Lo más importante para nosotros es tu presencia en este día especial...
+        Pero si algo gustas regalar, en efectivo lo sabremos apreciar.
+      </Typography>
+    </section>
+  );
 };
 
 export default Regalos;
